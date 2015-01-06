6c616ac6985df01adde75f81c911a7c3cc4c7be5
pacoavila24 <fco_avilavzqz@hotmail.com>
2015-01-05 20:20:19 -0600

    Update algorithm.cpp
    Arbol binario de busqueda basico

--- cpp/algorithm.cpp
+++ cpp/algorithm.cpp
@@ -1,2 +1,35 @@
 ////////////////////Primer ejercicio/////////////////////////////
 Describe un algoritmo para poder realizar una busqueda basica en un b-tree
+#include <iostream>
+#include <cstdlib>
+
+struct Node {
+   // Este es el consturctor, inicializa el Node
+   Node(int d): data(d), left(NULL), right(NULL) {} 
+   int data;
+   Node* left;
+   Node* right;
+};
+
+int main() {
+   // raiz es la raíz del árbol
+   Node* raiz = new Node(5);
+   raiz->left = new Node(3);
+   raiz->left->left = new Node(1);
+   raiz->left->right = new Node(4);
+   raiz->right = new Node(7);
+   raiz->right->right = new Node(9);
+   system("PAUSE");
+}
+
+// funcion que inserta en el b-tree
+Node* insert( Node* &branch, Node* new) {
+   if ( ! branch ) 
+      branch = new;
+   else if ( new->data < branch->data )
+      insert( branch->left, new ); 
+   else if ( new->data > branch->data )
+      insert( branch->right, new );
+   else
+      assert( false );
+}
