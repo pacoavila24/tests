72d876f0f18eea17c4ebe0d961f4bf8f0218ef7f
pacoavila24 <fco_avilavzqz@hotmail.com>
2015-01-05 20:53:13 -0600

    Update test2.cpp

--- cpp/test2.cpp
+++ cpp/test2.cpp
@@ -2,3 +2,6 @@
 class Test2
 {
 };
+
+//Pediria una referencia al main ya que se necesita la funcion main
+//En caso de tener el main en otra clase, el programa compilara pero esta clase no hara nada puesto que esta vaci
